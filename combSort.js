b6e6832f9648158d1fe528209aa6bf6bc4d95483
ayva <olga.ayva@gmail.com>
2015-08-31 09:00:38 -0700

    combSort

--- combSort.js
+++ combSort.js
@@ -1,5 +1,31 @@
 var combSort = function(arr){
 
-  // Your code here.
+ var swapped = true;
+ //Comb sort additional params
+ var gap = arr.length;
+ var shrink = 1.3;
+ 
+    // Repeat this loop until we have not had to swap values
+    while (swapped && gap !== 1){
+      swapped = false;
+      //Decrease gap after each iteration by dividing on shrink
+      gap = gap/shrink
+      gap = Math.ceil(gap)
+      
+      // Iterate through the array as in bubble, but compare elements with distance = gap
+      for(var i = 0; i + gap <= arr.length; i++){
+        // If a given value is more than the next, swap them
+        if (arr[i] > arr[i+gap]){
+          swapped = true;
+          var tmp = arr[i];
+          arr[i] = arr[i+gap];
+          arr[i+gap] = tmp;
+        }
+      }
+      
+ 
+      
+    }
+    return arr;
 
 };
